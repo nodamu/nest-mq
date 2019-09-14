645a58a6b39d82fff775ff5540d9c88fc417236b
Nicholas <nickadamu@gmail.com>
2019-09-14 14:47:00 +0000

    First commit

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Logger } from '@nestjs/common';
+import { AppService } from './app.service';
+import { EventPattern} from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  //   @Get()
+  //   getHello(): string {
+  //     return this.appService.getHello();
+  //   }
+  // }
+  @EventPattern('message_detected')
+  async onEvent(data: Record<string, unknown>) {
+    console.log('EVENT');
+  }
+
+
+}
